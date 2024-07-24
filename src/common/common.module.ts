75134725f7a7e1eaeec50b5ccd16fdf8f3adf042
Dzaky Abdurahman <100127440+dzakyabdurhmn@users.noreply.github.com>
2024-07-24 11:06:43 +0700

    fix : prisma service with onmodule init #1
    in this commit i implement common module, in this module i write config and winston to implement in globaly

---
+++ src/common/common.module.ts
@@ -0,0 +1,20 @@
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+
+@Global( )
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    WinstonModule.forRoot({
+      format: winston.format.json(),
+      transports: [new winston.transports.Console()],
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class CommonModule {}
