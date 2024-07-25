bd1d53897ca84472dc8507a47690fc3beb31ce2a
Dzaky Abdurahman <100127440+dzakyabdurhmn@users.noreply.github.com>
2024-07-25 09:38:10 +0700

    fix : somefile
     - package.json
     - common.module
     - common.service
     - validation.service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
-<<<<<<< Updated upstream
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [CommonModule, UserModule],
-=======
-
-@Module({
-  imports: [CommonModule],
->>>>>>> Stashed changes
   controllers: [],
   providers: [],
 })
--- src/common/validation.service.ts
+++ src/common/validation.service.ts
@@ -1,21 +1,9 @@
-<<<<<<< Updated upstream
 import { Injectable } from '@nestjs/common';
 import { ZodType } from 'zod';
 
-Injectable();
-export class ValidationService {
-  validate<T>(ZodType: ZodType<T>, data: T): T {
-    return ZodType.parse(data);
-  }
-}
-=======
-import { Injectable } from "@nestjs/common";
-import { ZodType } from "zod";
-
 @Injectable()
 export class ValidationService {
-    validate<T>(zodType: ZodType<T>, data: T): T{
-        return zodType.parse(data)
-    }
+  validate<T>(zodType: ZodType<T>, data: T): T {
+    return zodType.parse(data);
+  }
 }
->>>>>>> Stashed changes
